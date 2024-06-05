d60f21eee0cf1459fa5a5f4e531dc352a93cff02
Wiktor Napierała <wiktor.napierala@outlook.com>
2024-06-05 14:29:24 +0200

    Code organization + ConfigureAwait

--- src/Wiknap.Testcontainers.MailServer/MailServerContainer.cs
+++ src/Wiknap.Testcontainers.MailServer/MailServerContainer.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 using DotNet.Testcontainers.Containers;
 
 using Microsoft.Extensions.Logging;
@@ -21,16 +23,23 @@ public sealed class MailServerContainer : DockerContainer
 
     public async Task AddEmailAsync(string email, string password, CancellationToken ct)
     {
-        await ExecAsync(MailServerSetupCommands.AddEmail(email, password), ct);
-        var now = DateTime.Now;
-        // After adding the user, we need to wait for a server to reconfigure.
-        // For now, I unfortunately didn't find other way than waiting for logs related to it
-        while (true)
-        {
-            var (stdout, _) = await GetLogsAsync(since: now.AddSeconds(1), until: DateTime.Now, ct: ct);
+        await ExecAsync(MailServerSetupCommands.AddEmail(email, password), ct).ConfigureAwait(false);
+        var since = DateTime.Now.AddSeconds(1);
 
-            if (stdout.Contains(".zoo"))
+        var stopWatch = new Stopwatch();
+        stopWatch.Start();
+        while (stopWatch.Elapsed <= TimeSpan.FromSeconds(5))
+        {
+            if (await WasConfigurationReloadedAsync(since, ct).ConfigureAwait(false))
                 return;
         }
+    }
+
+    // After adding the user, we need to wait for a server to reconfigure.
+    // For now, I unfortunately didn't find other way than waiting for logs related to it
+    private async Task<bool> WasConfigurationReloadedAsync(DateTime since, CancellationToken ct)
+    {
+        var (stdout, _) = await GetLogsAsync(since: since, until: DateTime.Now, ct: ct).ConfigureAwait(false);
+        return stdout.Contains(".zoo");
     }
 }
